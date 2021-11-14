a3ded3d25658b9765ee1f7faa2c7f8de6ad93f41
seongwoo <he1236@ajou.ac.kr>
2021-11-14 22:51:06 +0900

    ing

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("laalalala");
+//js로 변수선언 - const = constant = 상수 = 값이 바뀌지 않고 고정
+const a = 5  
+const b = 2
+
+
+console.log("laalalala"); // 브라우저 콘솔에 log(메시지) 넘기기
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+
