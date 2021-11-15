d72d80654369e722e779d1681b44d3ac4edb3f9c
seongwoo <he1236@ajou.ac.kr>
2021-11-15 20:48:35 +0900

    c13 array ing

---
+++ c13_array.js
@@ -0,0 +1,11 @@
+//Arrays : 배열 => 자료구조 : 우리가 어떻게 최선의 방법으로 자료를 정리할지에 대한 방법 및 구조
+
+/// Array : 일련의 자료를 선형의 자료구조인 리스트로 나열해 구조화
+//// 리스트의 개별 요소간 타입이 동일하지 않아도 된다. 짬뽕가능
+/// const 변수명 = ["A" , "B" , "C" , "D"]
+
+const daysOfWeek = ['mon','tue','wed','thur','fri','sat','sun'];
+
+const nonsense = [1,2,'hello',false,null,true,undefined]
+
+console.log(daysOfWeek,nonsense)
