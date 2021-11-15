209bf0e2c299ab45197df4f10cfb173c98b1625c
seongwoo <he1236@ajou.ac.kr>
2021-11-15 19:01:13 +0900

    Finish dataType

---
+++ c12_typeOthers.js
@@ -0,0 +1,20 @@
+//boolean
+const amIFat = true;
+const amIThin = false;
+
+console.log(amIFat);
+console.log(amIThin);
+
+//null - 값이 없음을 의미
+///null은 절대 자연발생하지 않는다
+////즉 null을 할당한다는 것은, 변수 내에 값이 존재하지 않는다는 것을 JS에게 의도적으로 보이기 위해 사용하는 것이다.
+///파이썬에서는 None으로 표현됨
+const nullV = null;
+console.log(nullV);
+
+//undefined - 변수를 선언하고 값을 대입하지 않은 경우의 상태 
+/// 변수의 메모리공간은 할당됐지만 그 안에 값이 들어가지 않은상태
+let unDefined;
+console.log(unDefined);
+
+
