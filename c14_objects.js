d677fc4bd56153772421b9c3aa36528e8cf1f3ed
seongwoo <he1236@ajou.ac.kr>
2021-11-17 03:51:22 +0900

    finish c16_function2.js

--- c14_objects.js
+++ c14_objects.js
@@ -6,15 +6,15 @@
 //// object : 한가지 객체가 가지는 속성들을 구조화  <-> list : 리스트 내 모든값이 같은 의미를 가짐, ex)요일, 수열, 장바구니 등등
 ///object를 사용O
 
-//object를 초기화 하기위해 중괄호를 사용한다
+//object를 초기화 하기 위해 중괄호를 사용한다
 /// const objectName = { prop1 : "val1", prop2 : 123, prop3 : true ... } 
-const player = {
+const player = { 
     name : "horiz.d",
-    age : 26,
+    age : 26, 
     stacks : ["R","Python","JS","HTML","css"],
-    money : 0
+    money : 0 
 };
-
+    
 // Object내의 속성에 접근하기
 ///(1) object.property 구조를 이용해 객체내 속성을 특정하여 접근가능
 console.log(player);
---
+++ c16_function2.js
@@ -0,0 +1,47 @@
+//fucntion의 매개변수,인자
+///매개변수명을 설정하면 해당 자리에 들어온 인자는 해당 매개변수명을 함수의 명령부에서 변수이름으로 사용한다.
+
+////사용자정의함수 ex1
+function sayHello(nameOfPerson, age){
+    console.log("Hello my name is " + nameOfPerson + " and I'm "+age+" years old.");
+};
+
+sayHello("cho",26);
+sayHello("yang",22);
+sayHello("paul",24);
+
+////사용자정의함수 ex2 
+function plus(firstNumber,secondNumber){
+    console.log(firstNumber+secondNumber);
+};
+
+plus(10,12);
+plus(); //NaN 출력 (Not A Number)
+
+
+//Object 내의 함수(모듈) 생성,사용
+///this 는 해당 모듈을 품고있는 객체명을 지정
+
+//// ex1 : this 활용
+const player = {
+    name : "Zed",
+    age : 23,
+    playerHello : function() {
+        console.log("Hello "+ this.name + " I'm Developer. You inserted "+this.age+" in your age Property.");
+    }
+}
+
+player.playerHello()
+
+//// ex2 : 외부변수를 인자로
+const user = {
+    name : "seongwoo",
+    age : 26,
+    Hello : function(targetPersonName){
+        console.log("Hello " + targetPersonName + ", I'm " + this.name +".");
+    }
+};
+
+user.Hello("jinx");
+user.Hello("VI");
+
